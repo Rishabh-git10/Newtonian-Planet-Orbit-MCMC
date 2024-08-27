1284d347cac0b6bad240894b419ad85fdd291dcc
Rishabh Dewangan <romantimeless@gmail.com>
2024-08-28 01:34:57 +0530

    chore: data generated

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 import seaborn; seaborn.set()
 
+def make_data(intercept, slope, N=20, dy=2, rseed=42):
+    rand = np.random.RandomState(rseed)
+    x = 100 * rand.rand(N)
+    y = intercept + slope * x
+    y += dy * rand.randn(N)
+    return x, y, dy * np.ones_like(x)
+
+theta_true = [2, 0.5]
+x, y, dy = make_data(theta_true[0], theta_true[1])
+
